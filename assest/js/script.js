65bda68b2c01cee63fdede155286fd2af89a285e
navnitkumar1122 <kumarnavnit482@gmail.com>
2023-07-24 16:06:48 +0530

    AgsMovers update 2nd time

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,8 +1,15 @@
-let actvlink = document.querySelectorAll('.leftgroup>ul>li>a')
-
-actvlink.forEach(a => {
-    a.addEventListener('click', function(){
-        actvlink.forEach(anc => anc.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
+//mega menu active on click section start//
+function showhide()
+{
+    let click = document.getElementById("mega-menu")
+    if(click.style.display = "none")
+    {
+        click.style.display = "block"; 
+        // console.log("yes");
+    }
+    else
+    {
+        click.style.disply = "none";
+    }
+}
+//mega menu active on click section end//
